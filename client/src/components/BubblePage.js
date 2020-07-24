492cc26b08e4b4947f445c02b38730915172b1d8
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-07-24 12:06:47 -0500

    ✅ STAGE 2: BubblePage gets colors on render

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,12 +8,27 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/api/colors")
+      .then((res) => {
+        // console.log('BubblePage -> res.data', res.data);
+        setColorList(res.data);
+      })
+      .catch((err) => {
+        console.log(
+          "BubblePage -> err",
+          err.response.status,
+          err.response.statusText
+        );
+      });
+  }, []);
 
   return (
-    <>
+    <div className='BubblePage container'>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
-    </>
+    </div>
   );
 };
 
