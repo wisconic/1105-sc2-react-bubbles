ae786152e5abd05f366c424866c1d8206ef92011
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-07-24 11:12:53 -0500

    🛠 SET UP: header as own component

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className='header container'>
+      <h1>Welcome to the Bubble App!</h1>
+    </div>
+  );
+};
+
+export default Header;
